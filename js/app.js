572c991526592a5af3e4e7fbc6ea6a49b10b6dd4
salma abusier <salma4567omer@gmail.com>
2025-06-09 16:17:34 +0300

    k

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 function addTask() {
+
     const input = document.getElementById("taskInput");
         const taskText = input.value.trim();
 
@@ -8,30 +9,42 @@ function addTask() {
         li.textContent = taskText;
 
         const statusContainer = document.createElement("div");
-        statusContainer.style.marginLeft = "auto";
-        statusContainer.style.display = "flex";
-        statusContainer.style.gap = "10px";
-        statusContainer.style.alignItems = "center";
-
+        statusContainer.className = "listelement";
+        
+        /* The Radio Button 1 */
         const doneLabel = document.createElement("label");
         const doneRadio = document.createElement("input");
+
         doneRadio.type = "radio";
         doneRadio.name = `status-${Date.now()}`; // Unique name for each task
+
         doneLabel.appendChild(doneRadio);
+        /* Namde The Button */
         doneLabel.appendChild(document.createTextNode("Done"));
 
+        /* The Radio Button 2 */
         const progressLabel = document.createElement("label");
         const progressRadio = document.createElement("input");
+
         progressRadio.type = "radio";
+        /* The Two Radio Masts Have The Same Name*/
         progressRadio.name = doneRadio.name;
+
         progressLabel.appendChild(progressRadio);
+        /* Namde The Button */
         progressLabel.appendChild(document.createTextNode("Progress"));
 
+        /* Attribution Of The Son (Two Radio) To The Father (Div) */
         statusContainer.appendChild(doneLabel);
         statusContainer.appendChild(progressLabel);
 
+        /* Attribution Of The (Div) To The (Li Mean List) */
         li.appendChild(statusContainer);
+ 
+        /* Attribution Of The (Li Mean List) To The (Ul) */
+        const ul = document.getElementById ("taskList");
+        ul.appendChild(li);
 
-        document.getElementById("taskList").appendChild(li);
+        /* Return The Input Fild Empty*/
         input.value = "";
     }
